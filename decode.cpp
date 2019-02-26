145d02887d3a9d9363de96862b605d0b9ee3dac1
Harrison  DeWitt <harrisondewitt@Harrisons-MacBook-Pro.local>
2019-02-25 18:26:18 -0800

    Fix lsls decode

--- decode.cpp
+++ decode.cpp
@@ -81,7 +81,7 @@ ALU_Ops decode (const ALU_Type data) {
   if (data.instr.lsli.op == ALU_LSLI_OP) {
     // 315: insert code here to print lsls instruction
     if (opts.instrs) { 
-      cout << "lsls r" << data.instr.lsli.rd  << ", r" << data.instr.lsli.rn << ", r" << data.instr.lsli.imm << endl;
+      cout << "lsls r" << data.instr.lsli.rd  << ", #" << data.instr.lsli.imm << endl;
     }
     return ALU_LSLI;
   }
